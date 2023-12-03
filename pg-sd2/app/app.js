4cf47566fda960eca005d262dc629f7404273887
Susan Olofin <Susan Olofin>
2023-12-03 18:54:02 +0000

    updated pug template for list of walking partners

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -1,10 +1,10 @@
 // Import express.js
-// Use the Pug templating engine
-
 const express = require("express");
 
 // Create express app
-var app = express();
+const app = express();
+
+// Set up Pug templating engine
 app.set('view engine', 'pug');
 app.set('views', './app/views');
 
@@ -14,53 +14,49 @@ app.use(express.static("static"));
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-    var test_data = ['one', 'two', 'three','four'];
-    res.render("index", {'title':'my index page','heading':'my heading'});
-});
-
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+// Create a route for the root - /
+app.get("/", async function (req, res) {
+    try {
+        // Replace this with your actual logic to fetch walking buddies from the database
+        const walkingPartners = await fetchWalkingPartners();
+        
+        res.render("walkBuddies", { title: 'Walking Buddies', walkingPartners });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 // Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
+app.get("/goodbye", function (req, res) {
     res.send("Goodbye world!");
 });
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
+// Create a dynamic route for /hello/<name>
+app.get("/hello/:name", function (req, res) {
     // req.params contains any parameters in the request
     // We can examine it in the console for debugging purposes
     console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
+    // Retrieve the 'name' parameter and use it in a dynamically generated page
     res.send("Hello " + req.params.name);
 });
 
-
-/*const express = require('express');
-const app = express();
-const port = 3000;*/
-
-app.set('view engine', 'pug'); // Set Pug as the view engine
-
+// Create a route for /login
 app.get('/login', (req, res) => {
-  // You can pass data to the template using an object
-  res.render('login', { pageTitle: 'Login Page' });
+    // You can pass data to the template using an object
+    res.render('login', { pageTitle: 'Login Page' });
 });
 
+// Function to fetch walking partners - replace this with your actual database query
+async function fetchWalkingPartners() {
+    return [
+        { name: 'John Doe', bio: 'A fellow working mom', compatibility: 80, id: 1 },
+        // Add more walking partners as needed
+    ];
+}
 
 // Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+const port = 3000;
+app.listen(port, function () {
+    console.log(`Server running at http://127.0.0.1:${port}/`);
 });

92319a709ec7eacd097dd5197c4c8e3f786fa697
Susan Olofin <Susan Olofin>
2023-12-08 15:59:46 +0000

    I added the content in search results pug template

--- 0x0_lets_walk/app/app.js
+++ 0x0_lets_walk/app/app.js
@@ -1,4 +1,4 @@
-// Import express.js
+/*// Import express.js
 const express = require("express");
 
 // Create express app
@@ -48,4 +48,28 @@ app.get("/successful-selection", function (req, res) {
 const port = 3000;
 app.listen(port, function () {
     console.log(`Server running at http://127.0.0.1:${port}/`);
+});*/
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Set the view engine to Pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route to render your Pug template
+app.get('/', (req, res) => {
+  const walkingPartners = []; // Replace with your data
+  res.render('index', { walkingPartners });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
